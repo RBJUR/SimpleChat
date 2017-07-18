1d01381f81d97e13332cd34552c8f52efd86b60d
Roque Buarque Jr <roque@contro.ly>
2017-07-17 21:15:52 -0300

    unificando register

--- app/src/main/java/com/example/roquebuarquejr/simplechat/registration/view/RegisterActivity.java
+++ app/src/main/java/com/example/roquebuarquejr/simplechat/registration/view/RegisterActivity.java
@@ -4,27 +4,58 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import com.example.roquebuarquejr.simplechat.R;
 import com.firebase.client.Firebase;
 
-public class RegisterActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RegisterActivity extends AppCompatActivity implements  RegisterView  {
+
+    @BindView(R.id.register_edt_email)
+    EditText edtName;
+
+
+    @BindView(R.id.register_edt_email)
+    EditText edtEmail;
+
+    @BindView(R.id.register_edt_email)
+    EditText edtPassword;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
         Firebase.setAndroidContext(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        addStartFragment();
     }
 
-    private void addStartFragment() {
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).add(R.id.register_activity_frame_layout, new ChooseUsernameFragment(), "username").commit();
+    @Override
+    public void onSuccess() {
+        //TO DO CALL CHATSCREEN
+    }
+
+    @Override
+    public void onFailure() {
+
+    }
+
+    @Override
+    public void spinProgressBar() {
+
+    }
+
+    @Override
+    public void stopProgressBar() {
+
     }
 }
